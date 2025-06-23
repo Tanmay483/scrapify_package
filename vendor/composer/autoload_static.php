0231d9642636fe923b85eb1fc2ee817694536ce8
Tanmay0483 <keval.infinitysoftech@gmail.com>
2025-06-23 15:33:03 +0530

    Fixed issues of office to pdf

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -127,8 +127,8 @@ class ComposerStaticInit215f93f45de0b815067b0506775c91f6
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
